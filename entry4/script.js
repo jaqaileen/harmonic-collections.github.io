27ea031f51f9163f7b470e4c8a48e22977854b7a
jaqaileen <jq.pramana@gmail.com>
2023-09-26 23:43:41 -0400

    pg

---
+++ entry4/script.js
@@ -0,0 +1,18 @@
+
+const parallaxBg = document.querySelector('.parallax-bg');
+
+window.addEventListener('keydown', (e) => {
+    const currentX = parallaxBg.style.transform ? parseFloat(parallaxBg.style.transform.match(/-?\d+\.*\d*/)[0]) : 0;
+    const maxX = window.innerWidth;
+    const step = 50; // Adjust as needed for scrolling speed
+
+    if (e.key === 'ArrowRight' && currentX >= -maxX) {
+        parallaxBg.style.transform = `translateX(${currentX - step}px)`;
+    } else if (e.key === 'ArrowDown') {
+        window.scrollBy(0, step);
+    } else if (e.key === 'ArrowLeft' && currentX <= 0) {
+        parallaxBg.style.transform = `translateX(${currentX + step}px)`;
+    } else if (e.key === 'ArrowUp') {
+        window.scrollBy(0, -step);
+    }
+});
