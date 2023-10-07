727be671fde3530a71c716ce09a9dd9bc2c375fb
jaqaileen <jq.pramana@gmail.com>
2023-10-07 17:41:34 -0400

    changes

--- entry4/script.js
+++ entry4/script.js
@@ -1,16 +1,20 @@
 
 // Define the images to be found
 const imagesToFind = [
-    { src: "../assets/bluegem.png", top: '60px', left: '600px' },
-    { src: "../assets/browngem.png", top: '160px', left: '250px' },
+    { src: "../assets/bluegem.png", top: '531px', left: '616px' },
+    { src: "../assets/browngem.png", top: '480px', left: '605px' },
     { src: "../assets/greengem.png", top: '300px', left: '50px' },
+    { src: "../assets/lightgreengem.png", top: '310px', left: '626px' },
+    { src: "../assets/lightpinkgem.png", top: '285px', left: '776px' },
+    { src: "../assets/pinkgem.png", top: '351px', left: '898px' },
+    { src: "../assets/redgem.png", top: '590px', left: '985px' },
+    { src: "../assets/whitegem.png", top: '380px', left: '1300px' },
+    { src: "../assets/yellowgem.png", top: '574px', left: '946px' },
     
 ];
 
-// Initialize an array to track found images
 const foundImages = [];
 
-// Function to display images to find
 function displayImagesToFind() {
     const instructionBox = document.querySelector('.instruction-box');
     const gameBox = document.querySelector('.game-box');
@@ -19,38 +23,33 @@ function displayImagesToFind() {
         const imgElement = document.createElement('img');
         imgElement.src = image.src;
 
-        // Clone the image element for the game box
         const imgClone = imgElement.cloneNode(true);
 
         imgElement.style.top = image.top;
         imgElement.style.left = image.left;
 
-        // Add a click event listener to handle image found
         imgElement.addEventListener('click', () => {
             if (!foundImages.includes(image)) {
                 foundImages.push(image);
                 imgElement.remove();
-                imgClone.remove(); // Remove the clone from the game box
+                imgClone.remove();
                 checkGameCompletion();
             }
         });
 
         instructionBox.appendChild(imgClone);
-        gameBox.appendChild(imgElement); // Add the clone to the game box
+        gameBox.appendChild(imgElement); 
     }
 }
 
-// Function to check if the game is complete
 function checkGameCompletion() {
     if (foundImages.length === imagesToFind.length) {
         alert('Congratulations! You found all the images.');
     }
 }
 
-// Initialize the game
 function initGame() {
     displayImagesToFind();
 }
 
-// Call the game initialization function
 initGame();
